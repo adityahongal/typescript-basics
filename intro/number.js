bbe288ed6f83d0b1416d317aa9d202eb59e605e6
adityahongal <adityahongalwork@gmail.com>
2024-08-15 15:02:44 +0530

    Type Inferences number

---
+++ intro/number.js
@@ -0,0 +1,6 @@
+// Number
+// let userId : number = 56789.86  
+var userId = 56789.86; // Type Inference
+console.log(userId);
+// Boolean
+// let isLoggedIn : boolean = true
---
+++ intro/number.ts
@@ -0,0 +1,9 @@
+// Number
+
+// let userId : number = 56789.86  
+let userId = 56789.86          // Type Inference
+console.log(userId)
+
+// Boolean
+
+// let isLoggedIn : boolean = true
